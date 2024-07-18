56683260b76cae79c149f26dc5db6ed20b4dfa66
sadkowsr <rafal.sadkowski@gmail.com>
2024-07-18 12:57:14 +0200

    Update AvailableHoursWriteRepository.java

--- domain/src/main/java/org/sadkowski/cinema/domain/shows/ports/infrastructure/write/AvailableHoursWriteRepository.java
+++ domain/src/main/java/org/sadkowski/cinema/domain/shows/ports/infrastructure/write/AvailableHoursWriteRepository.java
@@ -1,6 +1,5 @@
 package org.sadkowski.cinema.domain.shows.ports.infrastructure.write;
 
-
 import org.sadkowski.cinema.domain.shows.model.AvailableHours;
 
 public interface AvailableHoursWriteRepository {
